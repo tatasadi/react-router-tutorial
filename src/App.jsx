a9ffbc7fccbfb52b9fb1e7bcbc77f7c7860ba117
Ehsan Tatasadi <ehsan@tatasadi.com>
2023-08-03 19:32:50 +0200

    move files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Layout from "../components/Layout"
-import About from "../pages/About"
-import Home from "../pages/Home"
+import Layout from "./components/Layout"
+import About from "./pages/About"
+import Home from "./pages/Home"
 
 function App() {
   return (
