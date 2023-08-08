1719770ecb6e71a1d88f0df0ca28be2b6a6af1e6
Ehsan Tatasadi <ehsan@tatasadi.com>
2023-08-08 15:49:01 +0200

    rename

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom"
 export default function Header() {
   return (
     <header className="bg-teal-900 text-teal-100 p-4 flex justify-between">
-      <h1>#MY-SHOP</h1>
+      <h1>#React-Router-Tutorial</h1>
       <nav className="flex gap-4">
         <NavLink to=".">Home</NavLink>
         <NavLink to="about">About</NavLink>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,7 +7,9 @@ export default function Layout() {
   return (
     <div className="flex flex-col h-screen">
       <Header />
-      <Outlet />
+      <div className="m-4 p-4 bg-white shadow  rounded-lg h-full">
+        <Outlet />
+      </div>
       <Footer />
     </div>
   )
