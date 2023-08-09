628e9ae904c46a6b55d2053b70f065d632f09d1a
Ehsan Tatasadi <ehsan@tatasadi.com>
2023-08-09 17:10:49 +0200

    add home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,41 @@
-import React from "react"
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <div>Home Page</div>
+  return <div className="bg-white">
+    <div className="relative isolate overflow-hidden bg-gradient-to-b from-teal-100/20 pt-14">
+      <div
+        className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-teal-600/10 ring-1 ring-teal-50 sm:-mr-80 lg:-mr-96"
+        aria-hidden="true"
+      />
+      <div className="mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
+          <h1 className="max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto">
+            React Router Example
+          </h1>
+          <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
+            <p className="text-lg leading-8 text-gray-600">
+              This is an open source porject, that is built with "React" and "React Router". You can go to the movie page and see different movies listed. You can filter or go to detail page of a movie by clicking on it.
+            </p>
+            <div className="mt-10 flex items-center gap-x-6">
+              <Link
+                to="movies"
+                className="rounded-md bg-teal-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-teal-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-teal-600"
+              >
+                See the movies
+              </Link>
+              <Link to="about" className="text-sm font-semibold leading-6 text-gray-900">
+                About us <span aria-hidden="true">→</span>
+              </Link>
+            </div>
+          </div>
+          <img
+            src="/homepage.jpg"
+            alt=""
+            className="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36"
+          />
+        </div>
+      </div>
+      <div className="absolute inset-x-0 bottom-0 -z-10 h-24 bg-gradient-to-t from-white sm:h-32" />
+    </div>
+  </div>
 }
